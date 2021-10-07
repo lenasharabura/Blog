3369ef2321870c05d61bfbe02b4380f992c2ecf3
Elena <lenasharabura@gmail.com>
2021-10-07 13:03:14 +0300

    detail done

--- comments/forms.py
+++ comments/forms.py
@@ -6,7 +6,10 @@ from comments.models import Comment
 class CommentForm(forms.ModelForm):
     comment = forms.CharField(label='Новый комментарий', widget=forms.Textarea(attrs={
         'class': 'form-control',
-        'placeholder': 'Введите ваш комментарий'}))
+        'placeholder': 'Введите ваш комментарий',
+        'cols': 20,
+        'rows': 3,
+    }))
 
     class Meta:
         model = Comment
