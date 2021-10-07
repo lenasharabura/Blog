3c618331f5ac832427aef0b9ace247f6d3e9b729
Elena <lenasharabura@gmail.com>
2021-10-07 19:03:21 +0300

    add fixtures

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -20,18 +20,18 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7yx%9#smw)@d83ds-5ctr$^@*8)vp9*lqz#$gj-@16ds1smmq%'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['myblog-lenasharabura.herokuapp.com']
 
 
 # Application definition
