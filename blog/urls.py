fa5e53b57bba0f250f5b7fe0016e1b2ed8af95d6
Elena <lenasharabura@gmail.com>
2021-10-13 15:05:14 +0300

    last changes

--- blog/urls.py
+++ blog/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('<int:pk>/', show_article, name='detail'),
     path('update/<int:pk>/', ArticleUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='delete'),
-    path('save_comment/', save_comment, name='save_comment'),
+    path('', save_comment, name='save_comment'),
     path('save_article/', save_article, name='save_article'),
     path('accounts/', include(('accounts.urls', 'accounts'))),
 ]
